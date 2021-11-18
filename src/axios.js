cae56e28f0298dadc2912de73aa00411ec0d264b
htpt2910 <hmc2910@gmail.com>
2021-11-18 14:08:01 +0700

    update axios

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8081",
+    baseURL: "https://obstacle-crossed-backend.herokuapp.com/",
 })
 
 instance.interceptors.response.use(
